d0d2fc9d35975ae9dcaa920c6b34414e62a600bb
Sanjeet Mijar <ssnjitm6@gmail.com>
2026-01-06 19:49:10 +0545

    home page done

---
+++ app/details.tsx
@@ -0,0 +1,25 @@
+import { ScrollView, StyleSheet } from "react-native";
+
+
+
+export default function Details() {
+  
+
+  return (
+    <ScrollView
+    contentContainerStyle={{
+      gap:16,
+      padding:16,
+    }}
+    >
+     
+
+    </ScrollView>
+  );
+}
+
+
+const styles =StyleSheet.create({
+
+
+})
